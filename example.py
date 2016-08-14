404fb3e0218a3eee82627607ab4fa9ed240848b4
Aditya Paliwal <adityasanjay.paliwal@iiitb.org>
2016-08-14 15:33:54 +0530

    Added example.py

---
+++ example.py
@@ -0,0 +1,8 @@
+import CFStat
+'''
+    Make changes below
+'''
+if __name__ == '__main__':
+    CFStat.getSubmissions('Petr', 1, True)
+    CFStat.getWeeklyStatistics('Petr', 1, True)
+    CFStat.compareUsers('Petr', 1, 'tourist', 1)
